7e8c74835261328337297cac118e72f2a42d6a8c
dungdb1 <badungbkhn@gmail.com>
2015-12-24 17:55:56 +0700

    commit

---
+++ WebTest/js/services.js
@@ -0,0 +1,12 @@
+var raServices=angular.module('raServices',['ngResource']);
+
+raServices.factory('StudentsService', ['$resource',
+
+    function($resource){
+
+        return $resource('http://localhost:8080/service/:call', {}, {
+            students: {method:'GET',params:{call:'students'},isArray:true},
+            student:{method:'GET',params:{call:'student'},isArray:false}
+        });
+
+    }]);
